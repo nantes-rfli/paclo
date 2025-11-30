ab5521975e6a7f645c80afc188f8544d9ccd01b5
nanto <nanto@nantos.net>
2025-11-30 23:35:41 +0900

    chore: run eastwood over src+test

---
+++ dev/eastwood_runner.clj
@@ -0,0 +1,9 @@
+(ns eastwood-runner
+  (:require
+   [eastwood.lint :as lint]))
+
+(defn -main [& _]
+  ;; Lint both source and tests. Adjust paths if you add new roots.
+  (lint/eastwood {:source-paths ["src" "dev"]
+                  :test-paths   ["test"]
+                  :linters      [:all]}))
