24aea1390700203ff8c1750de2bf548458eebd9f
nanto <nanto@nantos.net>
2025-12-01 23:39:43 +0900

    test: strengthen tls annotate and pcap idle helpers

--- test/paclo/tls_ext_test.clj
+++ test/paclo/tls_ext_test.clj
@@ -37,3 +37,32 @@
   (let [hex "17 03 03 00 01 00"
         ba  (hex->bytes hex)]
     (is (nil? (tls-ext/extract-sni ba)))))
+
+(deftest annotate-tls-sni-attaches-app
+  (let [hex "
+    16 03 03 00 43   01 00 00 3F   03 03
+    00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+    00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+    00
+    00 02   00 2F
+    01   00
+    00 14
+      00 00   00 10
+        00 0E
+          00 00 0B
+            65 78 61 6D 70 6C 65 2E 63 6F 6D
+  "
+        ba (hex->bytes hex)
+        pkt {:decoded {:l3 {:l4 {:type :tcp
+                                 :payload ba}}}}
+        annotate (deref #'tls-ext/annotate-tls-sni)
+        out (annotate pkt)]
+    (is (= :tls (get-in out [:decoded :l3 :l4 :app :type])))
+    (is (= "example.com" (get-in out [:decoded :l3 :l4 :app :sni])))
+    (is (re-find #"SNI=example.com"
+                 (get-in out [:decoded :l3 :l4 :app :summary])))))
+
+(deftest annotate-tls-sni-noop-when-non-tcp
+  (let [pkt {:decoded {:l3 {:l4 {:type :udp :payload (byte-array 0)}}}}
+        annotate (deref #'tls-ext/annotate-tls-sni)]
+    (is (= pkt (annotate pkt)))))
