fc9918ddbaacd2cf17abac036e1f4262056c95ad
nanto <nanto@nantos.net>
2025-12-02 13:03:28 +0900

    test: add pcap helper coverage

--- test/paclo/pcap_unit_test.clj
+++ test/paclo/pcap_unit_test.clj
@@ -20,3 +20,42 @@
       (is (nil? (f "   "))))
     (testing "nil stays nil"
       (is (nil? (f nil))))))
+
+(deftest idle-next-accumulates-and-breaks
+  (let [f (deref #'p/idle-next)]
+    (is (= {:idle 5 :break? false} (f 3 2 10)))
+    (is (= {:idle 12 :break? true} (f 5 7 10)))))
+
+(deftest apply-filter!-skips-when-no-filter
+  (let [calls (atom 0)
+        f (deref #'p/apply-filter!)]
+    (is (= :pcap (f :pcap {:filter nil})))
+    (is (= 0 @calls))))
+
+(deftest set-bpf-on-device-uses-lookupnet
+  (let [mask (atom nil)
+        called (atom false)]
+    (with-redefs [p/lookupnet (fn [_] {:mask 123})
+                  p/set-bpf-with-netmask! (fn [_ _ m] (reset! mask m) (reset! called true))]
+      (p/set-bpf-on-device! :pcap "en0" "tcp")
+      (is @called)
+      (is (= 123 @mask)))))
+
+(deftest set-bpf-on-device-fallbacks-to-zero-on-error
+  (let [mask (atom nil)]
+    (with-redefs [p/lookupnet (fn [_] (throw (RuntimeException. "boom")))
+                  p/set-bpf-with-netmask! (fn [_ _ m] (reset! mask m))]
+      (p/set-bpf-on-device! :pcap "en0" "udp")
+      (is (= 0 @mask)))))
+
+(deftest pkt-handler-normalizes-arities
+  (let [h0-called (atom 0)
+        h1-called (atom nil)
+        f (deref #'p/->pkt-handler)]
+    ((f nil) {:x 1}) ;; noop
+    ((f (fn [] (swap! h0-called inc))) {:x 1})
+    ((f (fn [m] (reset! h1-called m))) {:x 2})
+    (is (= 1 @h0-called))
+    (is (= {:x 2} @h1-called))))
+
+;; end of file
