dfc6051f4f1e52d44cb63626eb4fc5003968e7b4
nanto <nanto@nantos.net>
2025-12-09 15:11:06 +0900

    chore: format edn files in fmt script

--- dev/script/fmt.sh
+++ dev/script/fmt.sh
@@ -2,4 +2,5 @@
 set -euo pipefail
 clojure-lsp clean-ns --parallel
 clojure-lsp format --parallel
+clojure-lsp format --filenames deps.edn,dev/nvd-clojure.edn
 echo "Formatted & cleaned namespaces."
